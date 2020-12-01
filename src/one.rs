857ec91b8efb0964f922b5c3b11a7d986eb6e480
Tyler Reid <treid@kabbage.com>
2020-12-01 11:58:28 -0500

    split into files

---
+++ src/one.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+pub fn day_one() {
+    let expenses = fs::read_to_string("day1.txt")
+        .expect("oh no")
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    
+    'outer: for i in 0..expenses.len() {
+        'inner: for j in (0..expenses.len()).rev() {
+            if i == j {
+                continue 'inner;
+            }
+
+            //part one
+            // let sum = expenses[i] + expenses[j];
+            // if sum == 2020 {
+            //     println!("found it! {} + {} = {}", expenses[i], expenses[j], expenses[i] * expenses[j]);
+            //     break 'outer;
+            // }
+            //part two
+            'evenmoreinner: for k in 0..expenses.len() {
+                if i == k || j == k {
+                    continue 'evenmoreinner;
+                }
+
+                let sum = expenses[i] + expenses[j] + expenses[k];
+                if sum == 2020 {
+                    println!("found it! {} + {} + {} = {}", expenses[i], expenses[j], expenses[k], expenses[i] * expenses[j] * expenses[k]);
+                    break 'outer;
+                }
+            }
+        }
+    }
+}
