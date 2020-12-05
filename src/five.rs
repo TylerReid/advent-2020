300287fe7d6243311d13926d38c02ed18afcc5ad
Tyler Reid <jamestylerreid@gmail.com>
2020-12-05 10:58:19 -0500

    day five part two, sort of

--- src/five.rs
+++ src/five.rs
@@ -4,16 +4,25 @@ use std::ops::Range;
 
 pub fn day_five() {
     let file = File::open("input/day5.txt").unwrap();
-    let s = io::BufReader::new(file)
+    let seats = io::BufReader::new(file)
         .lines()
         .map(|x| x.unwrap())
         .map(|x| seat_number(&x))
         .collect::<Vec<_>>();
 
-    println!("max seat number is: {}", s.iter().max().unwrap());
+    let mut missing = Vec::<(u32, u32)>::new();
+    for r in 0..128 {
+        for c in 0..8 {
+            if !seats.contains(&(r, c)) {
+                missing.push((r, c));
+            }
+        }
+    }
+    println!("missing: {:?}", missing);
+    
 }
 
-fn seat_number(s: &str) -> u32 {
+fn seat_number(s: &str) -> (u32, u32) {
     let mut row = 0..128;
     let mut column = 0..8;
     for c in s.chars() {
@@ -35,7 +44,7 @@ fn seat_number(s: &str) -> u32 {
     }
     assert_eq!(row.start, row.end-1);
     assert_eq!(column.start, column.end-1);
-    row.start * 8 + column.start
+    (row.start, column.start)
 }
 
 fn upper_half(r: Range<u32>) -> Range<u32> {
