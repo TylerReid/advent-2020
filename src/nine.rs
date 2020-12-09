aa4d1842082bf24773e941196f7ee09a0090e6bc
Tyler Reid <treid@kabbage.com>
2020-12-09 10:18:29 -0500

    day nine part two

--- src/nine.rs
+++ src/nine.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::ops::Range;
 
 pub fn day_nine() {
     let data = fs::read_to_string("input/day9.txt")
@@ -10,15 +9,32 @@ pub fn day_nine() {
 
     let mut start: usize = 0;
     let mut end: usize = 25;
+    let found_index: usize;
+    let found_value: u64;
 
     loop {
         let preamble = &data[start..end];
         if !is_sum_of_two(preamble, data[end]) {
+            found_index = end;
+            found_value = data[end];
             println!("{} is not the sum of {:?}", data[end], preamble);
             break;
         }
         start += 1;
         end += 1;
+    }
+
+    'outer: for x in 0..found_index {
+        let mut sum = 0;
+        for y in x..found_index {
+            sum += data[y];
+            if sum == found_value {
+                let r = &data[x..y+1];
+                println!("found values: {:?}", &data[x..y+1]);
+                println!("weakness: {}", r.iter().min().unwrap() + r.iter().max().unwrap());
+                break 'outer;
+            }
+        }
     }
 }
 
