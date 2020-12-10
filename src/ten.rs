8e3fbf8fa51342efd42109b64f3ab2c7286903fd
Tyler Reid <treid@kabbage.com>
2020-12-10 13:59:27 -0500

    not working day ten

--- src/ten.rs
+++ src/ten.rs
@@ -5,77 +5,55 @@ pub fn day_ten() {
     let mut data = fs::read_to_string("input/day10.txt")
         .expect("oh no")
         .lines()
-        .map(|x| (x.parse().unwrap(), Vec::new()))
-        .collect::<Vec<(i32, Vec<usize>)>>();
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<i32>>();
 
     data.sort();
-    let max_laptop_jotlage = data.iter().last().unwrap().0 + 3;
+    let laptop_jotlage = data.iter().last().unwrap() + 3;
 
-    println!("max laptop jolts: {}", max_laptop_jotlage);
+    println!("laptop jolts: {}", laptop_jotlage);
     let mut one_jolt = 0;
     let mut three_jolt = 0;
     let mut last_joltage = 0;
-    for jolts in data.iter() {
-        let diff = jolts.0 - last_joltage;
+    for &jolts in data.iter() {
+        let diff = jolts - last_joltage;
         if diff == 1 {
             one_jolt += 1;
         }
         if diff == 3 {
             three_jolt += 1;
         }
-        last_joltage = jolts.0;
+        last_joltage = jolts;
     }
     //add one more to the three diff since the laptop is alway 3 more
     three_jolt += 1;
+    println!("{:?}", data);
+    println!("part one: {} * {} = {}", one_jolt, three_jolt, one_jolt * three_jolt);
 
-    println!("{} * {} = {}", one_jolt, three_jolt, one_jolt * three_jolt);
+    let mut edges =std::iter::repeat(vec![])
+        .take(data.len())
+        .collect::<Vec<Vec<usize>>>();
 
     for i in (0..data.len()).rev() {
-        let current = data[i].0;
+        let current = data[i];
         for j in (0..i).rev() {
-            let candidate = data[j].0;
+            let candidate = data[j];
             if current - candidate < 4 {
-                data[i].1.push(j);
+                edges[i].push(j);
             }
         }
     }
 
-    println!("{:?}", data);
-
-    println!("{:?}", number_of_paths(&data, &mut HashMap::new(), data.len() - 1));
-
-    // let mut combinations: u64 = 1;
-    // for i in (0..data.len()).rev() {
-    //     let mut possible = 0;
-    //     if i == 0 {
-    //         possible = 1;
-    //     } else {
-    //         'inner: for j in (0..i).rev() {
-    //             let current = data[i];
-    //             let candidate = data[j];
-    //             if data[i] - data[j] < 4 {
-    //                 println!("{} -> {}", candidate, current);
-    //                 possible += 1;
-    //             } else {
-    //                 break 'inner;
-    //             }
-    //         }
-    //     }
-        
-    //     if possible == 0 {
-    //         println!("{}", i);
-    //     }
-    //     combinations = combinations * possible;
-    // }
-    // println!("{}", combinations);
+    let mut cache = HashMap::new();
+    println!("{:?}", number_of_paths(&edges, &mut cache, data.len() - 1));
 }
 
-fn number_of_paths(data: &Vec<(i32, Vec<usize>)>, cached_paths: &mut HashMap<usize, u64>, i: usize) -> u64 {
+fn number_of_paths(data: &Vec<Vec<usize>>, cached_paths: &mut HashMap<usize, u64>, i: usize) -> u64 {
     if i == 0 {
         return 1;
     }
     let mut paths = 0;
-    for &x in data[i].1.iter() {
+    for &x in data[i].iter() {
         if cached_paths.contains_key(&x) {
             paths += cached_paths.get(&x).unwrap();
         } else {
@@ -86,3 +64,5 @@ fn number_of_paths(data: &Vec<(i32, Vec<usize>)>, cached_paths: &mut HashMap<usi
     }
     paths
 }
+
+//[1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31, 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49]
